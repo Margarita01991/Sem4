4781694a429992d5485bcdaee1b5deb3a1931972
Margarita <margarita27011991@gmail.com>
2022-08-16 10:40:02 +0300

    add task24_function

---
+++ task_function/Program.cs
@@ -0,0 +1,16 @@
+﻿// принимает на вход число A и выдает сумму чисел от 1 до A
+Console.Clear();
+int Cycle(int a)                              // используем функцию
+{
+    int sum  = 0;                             //так как нужно будет записывать сумму принимаем в еще одну переменную
+    for(int i = 1; i<=a; i++)                // используем цикл
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+Console.Write("Введите число A: ");
+int number = Convert.ToInt32(Console.Read()); //принимаем это число Convert.ToInt32(numberText[0].ToString())
+int sum = Cycle(number);
+Console.Write($"Результат = {sum}");
