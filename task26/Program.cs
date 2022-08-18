5eafb5717d1e74937fd4c5f41824e662964f8803
Margarita <margarita27011991@gmail.com>
2022-08-18 12:17:00 +0300

    add task26

--- task26/Program.cs
+++ task26/Program.cs
@@ -1,5 +1,6 @@
 ﻿// // Напишите программу, которая принимает на вход число 
 // //и выдаёт количество цифр в числе. 456 -> 3; 78 -> 2; 89126 -> 5
+
 // Console.WriteLine("введите число: ");
 // int A = Convert.ToInt32(Console.ReadLine());
 // int count = 0;
@@ -10,16 +11,32 @@
 // }
 // Console.WriteLine(count);
 
-Console.WriteLine("введите число: ");
-int Cycle (int A)
-{   int sum = 0; //count
-    while (A > 1)
+// Console.WriteLine("введите число: ");
+// double Cycle (double A)
+// {   int sum = 0; //count
+//     while (A > 1)
+//     {
+//        A = A/10; 
+//        Console.WriteLine(A/10);
+//        sum++;
+//     }
+//     return sum;
+// }
+
+// double A = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(Cycle(A));
+
+int Cycle(int a)                                                  //15
+{
+    int sum = 0;                                                  //0           1         3
+    for(int i = 1; i<=a; i++)                                     //1 < 15 ;    2 <  15   3 <  15
     {
-       A = A/10;
-       sum++;
+        sum = sum + i;                                            //0 + 1 = 1 ; 1+2 = 3   3+3 = 6
+        Console.WriteLine(sum);
     }
     return sum;
 }
+Console.Write("введите число A: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Результат работы программы = {Cycle(number)}");
 
-int A = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(Cycle(A));
